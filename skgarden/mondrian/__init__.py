3de37d79279683ccc0fcdd84fe1c7b6b55602bc9
Brian Burns <brianbandrew@gmail.com>
2022-06-04 10:53:53 -0700

    Change __init__.py files to include RiverMondrianForestRegressor

--- skgarden/mondrian/__init__.py
+++ skgarden/mondrian/__init__.py
@@ -1,7 +1,9 @@
 from .ensemble import MondrianForestClassifier
 from .ensemble import MondrianForestRegressor
+from .ensemble import RiverMondrianForestRegressor
 from .tree import MondrianTreeClassifier
 from .tree import MondrianTreeRegressor
 
-__all__ = ["MondrianForestClassifier", "MondrianForestRegressor",
-           "MondrianTreeClassifier", "MondrianTreeRegressor"]
+__all__ = ["MondrianForestClassifier", "MondrianForestRegressor", 
+            "RiverMondrianForestRegressor", "MondrianTreeClassifier", 
+            "MondrianTreeRegressor"]
--- skgarden/mondrian/ensemble/__init__.py
+++ skgarden/mondrian/ensemble/__init__.py
@@ -1,4 +1,6 @@
 from .forest import MondrianForestClassifier
 from .forest import MondrianForestRegressor
+from .forest import RiverMondrianForestRegressor
 
-__all__ = ["MondrianTreeClassifier", "MondrianForestRegressor"]
+__all__ = ["MondrianTreeClassifier", "MondrianForestRegressor",
+		"RiverMondrianForestRegressor"]
